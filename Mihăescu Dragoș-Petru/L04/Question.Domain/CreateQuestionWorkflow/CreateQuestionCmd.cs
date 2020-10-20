67bb758b1c9da481474ad3a9cf03727b057e4984
dragosmihaescu98 <58433700+dragos155@users.noreply.github.com>
2020-10-20 21:59:00 +0300

    Tema4

---
+++ Mihăescu Dragoș-Petru/L04/Question.Domain/CreateQuestionWorkflow/CreateQuestionCmd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    public struct CreateQuestionCmd
+    {
+        [Required]
+        public string Title { get; private set; }
+        [Required]
+        public string Description { get; private set; }
+        public List<string> Tags { get; private set; }
+
+        public CreateQuestionCmd(string Title, string Description, List<string> Tags)
+        {
+            this.Title = Title;
+            this.Description = Description;
+            this.Tags = Tags;
+        }
+    }
+}
---
+++ Mihăescu Dragoș-Petru/L04/Test.App/Program.cs
@@ -0,0 +1,77 @@
+﻿using Question.Domain.CreateQuestionWorkflow;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using static Question.Domain.CreateQuestionWorkflow.CreateQuestionResult;
+
+namespace Test.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Tags = new List<string> { "JAVA", "C#" , "C++" };
+            var cmd = new CreateQuestionCmd("Ce este un constructor", "Cum creez un constructor automat?", Tags);
+            var result = CreateQuestion(cmd);
+             
+            result.Match(
+                ProcessQuestionCreated,
+                ProcessQuestionNotCreated,
+                ProcessInvalidQuestion
+                );
+
+            Console.ReadLine();
+
+        }
+        private static ICreateQuestionResult ProcessInvalidQuestion(QuestionValidationFailed validationErrors)
+        {
+            Console.WriteLine("Question validation failed:");
+
+            foreach (var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+
+            return validationErrors;
+        }
+        private static ICreateQuestionResult ProcessQuestionNotCreated(QuestionNotCreated questionNotCreatedResult)
+        {
+            Console.WriteLine($"Question not created: {questionNotCreatedResult.Reason}");
+
+            return questionNotCreatedResult;
+        }
+
+        private static ICreateQuestionResult ProcessQuestionCreated(QuestionCreated question)
+        {
+            Console.WriteLine($"Question {question.QuestionId}");
+
+            return question;
+        }
+        public static ICreateQuestionResult CreateQuestion(CreateQuestionCmd createQuestionCommand)
+        {
+            if (string.IsNullOrWhiteSpace(createQuestionCommand.Title)) 
+            {
+                var errors = new List<string>() { "Invlaid Title" };
+
+                return new QuestionValidationFailed(errors);
+            }
+
+            if (string.IsNullOrWhiteSpace(createQuestionCommand.Description))
+            {
+                var errors = new List<string>() { "Invlaid Description" };
+
+                return new QuestionValidationFailed(errors);
+            }
+
+            if (new Random().Next(10) > 1)
+            {
+                return new QuestionNotCreated("Question could not be verified");
+            }
+            var questionId = Guid.NewGuid();
+
+            var result = new QuestionCreated(questionId, createQuestionCommand.Title, createQuestionCommand.Description, createQuestionCommand.Tags);
+
+            return result;
+        }
+    }
+}
